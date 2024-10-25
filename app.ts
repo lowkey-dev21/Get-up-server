13d6cb0ab917bc4a648ed7cec2bbb7c52c49efae
Lowkey Dev <muizoyetola8@gmail.com>
2024-10-25 19:50:48 +0100

    Morgan changes

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import helmet from "helmet";
 import "dotenv/config";
 import { mongoDB } from "./db/db";
 import userRoute from "./routes/auth/user.route";
-import morgan from "morgan"
+import morgan from "morgan";
 
 const PORT: string | undefined = process.env.PORT;
 const app: Express = express();
@@ -13,10 +13,10 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
+
 if (process.env.NODE_SERVER === "development") {
   app.use(morgan("dev"));
 }
-
 
 app.use("/auth", userRoute);
 
